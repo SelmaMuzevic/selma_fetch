8d8fefe28fb102c411c0d64a5ae7c19ce804a356
selma <selma@gmail.com>
2017-08-22 11:51:53 +0200

    rajout test npm et corrections requette xhr

---
+++ index.js
@@ -0,0 +1,20 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+function fetch(url) {
+    return new Promise(function(ok, ko) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = function() {
+            if (xhr.status == 200) {
+                ok(xhr.responseText);
+            } else {
+                ko(xhr.status)
+            }
+        };
+        xhr.onerror = function(error) {
+            ko(error);
+        }
+        xhr.send();
+    });
+};
+module.exports = fetch;
